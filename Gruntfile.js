43c6faf3d8bdbb6611dfc34246f45de5785facdc
Oskar Risberg <kokarn@gmail.com>
2015-02-17 16:31:35 +0100

    Added Wizards

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,8 @@ module.exports = function( grunt ) {
                     'web/ingame-logo/avantgarde.png' : 'teams/Avant Garde/logo-292x292.png',
                     'web/ingame-logo/trident.png' : 'teams/TRIDENT/logo-221x221.png',
                     'web/ingame-logo/volgare.png' : 'teams/VOLGARE/logo-200x200.png',
-                    'web/ingame-logo/x6tence.png' : 'teams/x6tence/logo-170x170.png'
+                    'web/ingame-logo/x6tence.png' : 'teams/x6tence/logo-170x170.png',
+                    'web/ingame-logo/wizards.png' : 'teams/Wizards/logo-188x188.png'
                 }
             }
         }
