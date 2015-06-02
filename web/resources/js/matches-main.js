aca4243673b71b0f3e21ad688ae74322d4b79f12
Oskar Risberg <kokarn@gmail.com>
2015-06-02 18:18:48 +0200

    Minor code formatting cleanup

--- web/resources/js/matches-main.js
+++ web/resources/js/matches-main.js
@@ -1,24 +1,23 @@
-Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
-
-    switch (operator) {
+Handlebars.registerHelper( 'ifCond', function( v1, operator, v2, options ){
+    switch( operator ){
         case '==':
-            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+            return ( v1 == v2 ) ? options.fn( this ) : options.inverse( this );
         case '===':
-            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+            return ( v1 === v2 ) ? options.fn( this ) : options.inverse( this );
         case '<':
-            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+            return ( v1 < v2 ) ? options.fn( this ) : options.inverse( this );
         case '<=':
-            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+            return ( v1 <= v2 ) ? options.fn( this ) : options.inverse( this );
         case '>':
-            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+            return ( v1 > v2 ) ? options.fn( this ) : options.inverse( this );
         case '>=':
-            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+            return ( v1 >= v2 ) ? options.fn( this ) : options.inverse( this );
         case '&&':
-            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+            return ( v1 && v2 ) ? options.fn( this ) : options.inverse( this );
         case '||':
-            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+            return ( v1 || v2 ) ? options.fn( this ) : options.inverse( this );
         default:
-            return options.inverse(this);
+            return options.inverse( this );
     }
 });
 
